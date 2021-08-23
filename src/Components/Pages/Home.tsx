f115f98548bfdfde543eb42d60f7f5e9cf5311f0
willie <jaysimwanza@gmail.com>
2021-08-23 20:37:38 +0200

    Added TopBar to Project

---
+++ src/Components/Pages/Home.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import TopBar from '../../Common/TopBar'
+
+const Home = () => {
+    return (
+        <> 
+     <TopBar />
+        </>
+        
+        
+    )
+}
+
+export default Home
